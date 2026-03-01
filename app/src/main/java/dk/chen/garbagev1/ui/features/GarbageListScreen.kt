2d64af678a982d2237dce7704e96adb2aa8cba57
shichenitu <shichenitu@163.com>
2026-03-01 15:19:20 +0100

    GarbageListScreen.kt

---
+++ app/src/main/java/dk/chen/garbagev1/ui/features/GarbageListScreen.kt
@@ -0,0 +1,141 @@
+package dk.chen.garbagev1.ui.features
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dk.chen.garbagev1.R
+import dk.chen.garbagev1.domain.Item
+import dk.chen.garbagev1.domain.fullDescription
+import dk.chen.garbagev1.ui.theme.theme.GarbageV1Theme
+import kotlinx.serialization.Serializable
+
+@Serializable
+object GarbageGraph
+
+@Serializable
+object GarbageList
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GarbageListScreen(
+    onNavigate: (GarbageListViewModel.NavigationEvent) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: GarbageListViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.navigationEvents.collect {
+            onNavigate(it)
+        }
+    }
+
+    GarbageListScreen(
+        modifier = modifier,
+        uiState = uiState,
+        uiEvents = viewModel.uiEvents
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun GarbageListScreen(
+    uiState: GarbageListViewModel.UiState,
+    uiEvents: GarbageListViewModel.UiEvents,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(modifier = Modifier.padding(vertical = 16.dp)) {
+                    Text(
+                        text = "Garbage Sorting",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    uiState.garbageList.forEach { item ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(text = item.fullDescription())
+                            IconButton(onClick = { uiEvents.onEditItemClick(item) }) {
+                                Icon(
+                                    imageVector = Icons.Default.Edit,
+                                    contentDescription = "Edit item"
+                                )
+                            }
+                        }
+                    }
+                }
+
+                Button(onClick = uiEvents::onAddItemClick) {
+                    Text(text = "Add item")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GarbageListScreenPreview() {
+    GarbageV1Theme {
+        GarbageListScreen(
+            modifier = Modifier,
+            uiState = GarbageListViewModel.UiState(
+                garbageList = listOf(
+                    Item(what = "Can", where = "metal"),
+                    Item(what = "Book", where = "paper")
+                )
+            ),
+            uiEvents = object : GarbageListViewModel.UiEvents {
+                override fun onAddItemClick() {}
+                override fun onEditItemClick(item: Item) {}
+            },
+        )
+    }
+}
