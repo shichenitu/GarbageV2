2a10dbb1f6a78d4dd571d2e68407945600b951f2
shichenitu <shichenitu@163.com>
2026-03-08 10:35:24 +0100

    ui/features/garbage/GarbageListScreen.kt

--- app/src/main/java/dk/chen/garbagev1/ui/features/garbage/GarbageListScreen.kt
+++ app/src/main/java/dk/chen/garbagev1/ui/features/garbage/GarbageListScreen.kt
@@ -1,47 +1,46 @@
 package dk.chen.garbagev1.ui.features.garbage
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
 import dk.chen.garbagev1.R
 import dk.chen.garbagev1.domain.Item
-import dk.chen.garbagev1.domain.fullDescription
+import dk.chen.garbagev1.ui.components.GarbageTopAppBar
 import dk.chen.garbagev1.ui.components.ItemOrNullProvider
+import dk.chen.garbagev1.ui.components.NavigationType
 import dk.chen.garbagev1.ui.components.ThemedPreviews
+import dk.chen.garbagev1.ui.components.previewBins
 import dk.chen.garbagev1.ui.components.previewGarbageList
 import dk.chen.garbagev1.ui.navigation.AppRoute
 import dk.chen.garbagev1.ui.navigation.NestedGraph
 import dk.chen.garbagev1.ui.theme.theme.GarbageV1Theme
 import kotlinx.serialization.Serializable
-
-@Serializable
-object GarbageGraph
 
 @Serializable
 object GarbageSearch : NestedGraph {
@@ -52,88 +51,97 @@ object GarbageSearch : NestedGraph {
 @Serializable
 data class SortingList(val itemId: String? = null) : AppRoute
 
-@Serializable
-object GarbageList
-
-@Serializable
-object ListGroup
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GarbageListScreen(
-    onNavigate: (GarbageListViewModel.NavigationEvent) -> Unit,
-    modifier: Modifier = Modifier,
-    viewModel: GarbageListViewModel = hiltViewModel(),
+    uiState: GarbageListViewModel.UiState,
+    uiEvents: GarbageListViewModel.UiEvents,
+    modifier: Modifier = Modifier
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-    LaunchedEffect(key1 = Unit) {
-        viewModel.navigationEvents.collect {
-            onNavigate(it)
+    Scaffold(
+        topBar = {
+            GarbageTopAppBar(
+                titleRes = R.string.app_name,
+                navigationType = NavigationType.NONE
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = uiEvents::onAddItemClick) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.add_button_label)
+                )
+            }
+        }
+    ) { contentPadding ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues = contentPadding)
+        ) {
+            items(uiState.garbageList) { item ->
+                val shop = uiState.bins.find { it.name == item.where }
+                ListItem(
+                    item = item,
+                    imageUrl = shop?.imageUrl,
+                    binColor = shop?.binColor,
+                    onItemClick = { uiEvents.onEditItemClick(item) }
+                )
+            }
         }
     }
 
-    GarbageListScreen(
-        modifier = modifier,
-        uiState = uiState,
-        uiEvents = viewModel.uiEvents
-    )
+    uiState.selectedItem?.let {
+        DetailsSheet(
+            item = it,
+            isWhatError = uiState.isWhatError,
+            isWhereError = uiState.isWhereError,
+            showDeleteConfirmation = uiState.showDeleteConfirmation,
+            uiEvents = uiEvents
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun GarbageListScreen(
-    uiState: GarbageListViewModel.UiState,
-    uiEvents: GarbageListViewModel.UiEvents,
-    modifier: Modifier = Modifier,
-) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.app_name)) },
-                navigationIcon = {
-                    IconButton(onClick = uiEvents::onUpClick) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back"
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer
-                )
-            )
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+private fun ListItem(item: Item, imageUrl: String?, binColor: Color?, onItemClick: () -> Unit) {
+    Card(
+        onClick = onItemClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(all = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            item {
-                Button(onClick = uiEvents::onAddItemClick) {
-                    Text(text = "Add item")
-                }
+            Column(modifier = Modifier.weight(weight = 1f)) {
+                Text(
+                    text = item.what,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    color = binColor ?: MaterialTheme.colorScheme.onSurface
+                )
+                Text(
+                    text = item.where,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    color = binColor ?: MaterialTheme.colorScheme.onSurface
+                )
             }
 
-            items(uiState.garbageList) { item ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = item.fullDescription())
-                    IconButton(onClick = { uiEvents.onEditItemClick(item) }) {
-                        Icon(
-                            imageVector = Icons.Default.Edit,
-                            contentDescription = "Edit item"
-                        )
-                    }
-                }
+            // TODO Add shop logo
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = "Bin Logo",
+                modifier = Modifier.size(48.dp),
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                error = painterResource(R.drawable.ic_launcher_background)
+            )
+
+            IconButton(onClick = onItemClick) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = stringResource(id = R.string.edit_item_title)
+                )
             }
         }
     }
@@ -144,15 +152,28 @@ private fun GarbageListScreen(
 fun GarbageListScreenPreview(@PreviewParameter(provider = ItemOrNullProvider::class) itemOrNull: Item?) {
     GarbageV1Theme {
         GarbageListScreen(
-            modifier = Modifier,
             uiState = GarbageListViewModel.UiState(
                 garbageList = previewGarbageList(),
+                bins = previewBins(),
+                selectedItem = itemOrNull,
+                isWhatError = false,
+                isWhereError = false,
+                showDeleteConfirmation = false,
             ),
             uiEvents = object : GarbageListViewModel.UiEvents {
+                override fun onWhatChange(what: String) {}
+                override fun onWhereChange(where: String) {}
+                override fun onSaveClick(): Boolean {
+                    return true
+                }
+                override fun onDeleteClick() {}
+                override fun onConfirmDelete() {}
+                override fun onDismissDeleteConfirmation() {}
+                override fun onDismissDetails() {}
                 override fun onAddItemClick() {}
                 override fun onEditItemClick(item: Item) {}
                 override fun onUpClick() {}
-            },
+            }
         )
     }
 }
