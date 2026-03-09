85552c0e376eb5b7479102d63e33e310e4b9b4c4
shichenitu <shichenitu@163.com>
2026-03-09 21:43:43 +0100

    ui/theme/Theme.kt

--- app/src/main/java/dk/chen/garbagev1/ui/theme/theme/Theme.kt
+++ app/src/main/java/dk/chen/garbagev1/ui/theme/theme/Theme.kt
@@ -8,7 +8,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dk.chen.garbagev1.domain.Theme
+import dk.chen.garbagev1.ui.features.settings.SettingsViewModel
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -88,11 +93,16 @@ private val darkScheme = darkColorScheme(
 
 @Composable
 fun GarbageV1Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    viewModel: SettingsViewModel = hiltViewModel(),
     content: @Composable () -> Unit
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val darkTheme = when (uiState.theme) {
+        Theme.LIGHT -> false
+        Theme.DARK -> true
+        Theme.SYSTEM -> isSystemInDarkTheme()
+    }
+    val dynamicColor = false
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
