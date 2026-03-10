cfdbba6593fe2682c7392aa068c9eeb7ae0a863f
shichenitu <shichenitu@163.com>
2026-03-10 08:49:55 +0100

    ui/features/settings/SettingsScreen.kt

--- app/src/main/java/dk/chen/garbagev1/ui/features/settings/SettingsScreen.kt
+++ app/src/main/java/dk/chen/garbagev1/ui/features/settings/SettingsScreen.kt
@@ -1,35 +1,110 @@
 package dk.chen.garbagev1.ui.features.settings
 
-import dk.chen.garbagev1.R
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dk.chen.garbagev1.R
+import dk.chen.garbagev1.domain.Theme
 import dk.chen.garbagev1.ui.components.ThemedPreviews
 import dk.chen.garbagev1.ui.navigation.AppRoute
 import dk.chen.garbagev1.ui.theme.theme.GarbageV1Theme
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 object Settings : AppRoute
 
 @Composable
-fun SettingsScreen() {
-    Box(
+fun SettingsScreen(
+    viewModel: SettingsViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    SettingsScreen(uiState, viewModel.uiEvents)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsScreen(
+    uiState: SettingsViewModel.UiState,
+    uiEvents: SettingsViewModel.UiEvents
+) {
+    var expanded by remember { mutableStateOf(value = false) }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(all = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = stringResource(id = R.string.settings_screen_title),
-            color = MaterialTheme.colorScheme.onBackground
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(id = R.string.theme_label),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded }
+            ) {
+                TextField(
+                    value = uiState.theme.name.lowercase(),
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    modifier = Modifier.menuAnchor(
+                        type = MenuAnchorType.PrimaryNotEditable,
+                        enabled = true
+                    )
+                )
+                // TODO: Add an ExposedDropdownMenu & a DropdownMenuItem for each Theme.entries. NB: Theme names needs to be lowercase.
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    Theme.entries.forEach { theme ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(text = theme.name.lowercase())
+                            },
+                            onClick = {
+                                uiEvents.onSetTheme(theme)
+                                expanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
